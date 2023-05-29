d8c39a3799b0cf9e175445120ab1b1a52fb93c9d
Erik Plachta <eplachta@gmail.com>
2023-05-29 17:56:26 -0400

    Verified integrity.

--- src/app/sitemap/page.tsx
+++ src/app/sitemap/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Head from "next/head";
-import parseSitemapXml, {XmlEntry} from '@/utils/Sitemap';
+import parseSitemapXml, {SitemapXml} from '@/utils/Sitemap';
 
 import { capitalize } from '@/utils/String';
 import Link from 'next/link';
@@ -10,7 +10,8 @@ import Link from 'next/link';
 
 
 export default async function SiteMap() {
-  let sitemapData:XmlEntry[] = await parseSitemapXml();
+  
+  let sitemapData:SitemapXml[] = await parseSitemapXml();
   
   return(
     <>
@@ -19,7 +20,7 @@ export default async function SiteMap() {
       </Head>
       <section className='max-w-2xl px-8'>
         <div className='bg-transparent pb-10 grid grid-cols-1 gap-[1.5rem] md:grid-cols-2 relative z-10 rounded-lg w-full col-span-full'>
-            {sitemapData && sitemapData.map((urlData:XmlEntry, index) => (
+            {sitemapData && sitemapData.map((urlData:SitemapXml, index) => (
                 <div key={index}>
                     <h3 className='text-lg font-bold border-solid border-b-2 border-0 border-blue-900 mt-6 pb-2 mb-4'>
                         { urlData.route ? capitalize(urlData?.route) : "Home" }
--- src/utils/Sitemap.ts
+++ src/utils/Sitemap.ts
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import path from 'path';
 
-export type XmlEntry = {
+export type SitemapXml = {
   loc         : string;
   route       : string;
   lastmod     : string;
@@ -9,16 +9,21 @@ export type XmlEntry = {
 }
 
 /**
- * Parses the sitemap.xml file and returns an array of XmlEntry objects
+ * Parses the sitemap.xml file and returns an array of SitemapXml objects
  * 
- * @returns {XmlEntry[]} Array of XmlEntry objects
+ * @returns {SitemapXml[]} Array of SitemapXml objects
  */
-export default async function parseSitemapXml():Promise<XmlEntry[]> {
-  const filePath = path.join(process.cwd(), 'public', 'sitemap.xml');
-  const fileData = fs.readFileSync(filePath, 'utf8');
-  const matches = Array.from(
-              fileData.matchAll(/<url>[\s\S]*?<loc>(.*?)<\/loc>[\s\S]*?<route>(.*?)<\/route>[\s\S]*?<lastmod>(.*?)<\/lastmod>[\s\S]*?<\/url>/g), m => ({ loc: m[1], route: m[2], lastmod: m[3] }));
-  return matches as XmlEntry[]
+export default async function parseSitemapXml():Promise<SitemapXml[]> {
+  try {
+    const filePath = path.join(process.cwd(), 'public', 'sitemap.xml');
+    const fileData = fs.readFileSync(filePath, 'utf8');
+    const matches = Array.from(
+                fileData.matchAll(/<url>[\s\S]*?<loc>(.*?)<\/loc>[\s\S]*?<route>(.*?)<\/route>[\s\S]*?<lastmod>(.*?)<\/lastmod>[\s\S]*?<\/url>/g), m => ({ loc: m[1], route: m[2], lastmod: m[3] }));
+    return matches as SitemapXml[]
+    } catch (error) {
+      throw new Error('Error parsing sitemap.xml. Verify public/sitemap.xml exists and is valid XML.');
+      return [];
+    }
 };
 
 
