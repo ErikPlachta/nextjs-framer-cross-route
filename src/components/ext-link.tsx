f164882b8a4e18fc76cf35383bff911b00108194
Erik Plachta <eplachta@gmail.com>
2023-05-29 17:06:01 -0400

    Add ExtLink component.

---
+++ src/components/ext-link.tsx
@@ -0,0 +1,25 @@
+
+/**
+ * External Link component that opens in a new tab. 
+ * 
+ * @param {Object} props - props object
+ * @param {String} props.target - target attribute
+ * @param {String} props.rel - rel attribute
+ * @param {String} props.href - href attribute
+ * 
+ * @returns {JSX.Element} - JSX Element
+ * 
+ * @example
+ * import ExtLink from 'components/ext-link'
+ * 
+ * <ExtLink href="https://example.com" target="_blank" rel="noopener">
+ * 
+ */
+export default function ExtLink(props = { target: String() || '_blank' }):JSX.Element{
+  return (
+    <a {...props} rel="noopener" target={props.target || '_blank'} />
+  )
+}
+
+
+
