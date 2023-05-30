8174fe038033a031fd96eceae2b997912a618361
Erik Plachta <eplachta@gmail.com>
2023-05-30 18:10:00 -0400

    Add base content and verified.

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,52 +1,116 @@
-import Image from "next/image";
+// import Image from "next/image";
+"use client";
 import { motion } from "framer-motion";
+import getPostData from '@/context/blog/posts';
+import Link from "next/link";
+import { useSearchParams } from "next/navigation";
 
 export default function Page() {
+  //-- simulate query to database
+  let data = getPostData();
   
-  let data = testBlogData;
+  //-- Extracting where to navigating to 
+  const router = useSearchParams();
+  const searchParams = useSearchParams();
+  let postIdRoutingTo = searchParams.get("id");
+
   
   function buildBlogData():any{
     
-    return testBlogData.map((post) => {
-      return (
-        <div className='border-solid border-1 border-[rgba(0,0,0, .1)] p-4 rounded bg-primary'>
-          <h3>{post.title}</h3>
-          {/* <span className='overflow-hidden h-[100px] max-h-[200px]'> */}
-          <div
-            // layoutId={`photo-${place.id}`}
-            className={`relative bg-gradient-to-tr ${post.blend} overflow-hidden rounded-md`}
-            // transition={{ ease: "easeOut" }}
-            // initial={{ 
-            //   height: imageHeightFrom,
-            // }}
-            // animate={{ 
-            //   height: imageHeightTo.current,
-            // }}
-            // style={{ originX: 0.5 }}
+    return (
+      data.map((post) => {
+        return (
+          <Link
+            key={post.id}
+            href={`/blog/${post.id}`}
+            passHref
+            scroll={false}
+            legacyBehavior
           >
-            <img
-              // layoutId={`image-${place.id}`}
-              // transition={{ ease: "easeInOut" }}
-              src={post.image}
-              alt={post.title}
-              className="absolute w-full object-cover mix-blend-soft-light"
-              // initial={{
-              //   height: imageHeightFrom,
-              // }}
-              // animate={{
-              //   height: imageHeightTo.current,
-              // }}
-              // style={{
-              //   originX: 1,
-              //   objectPosition: place.position,
-              // }}
-            />
-          </div>
-          
-        </div>
+                
+          {/** Parent Element around each post.
+           * 
+          */}
+          <motion.a
+            // className="relative block mx-2 overflow-hidden rounded shadow-[1px_1px_2px_1px_rgba(0,0,0,0.4)]"
+            className="relative block mx-2 overflow-hidden rounded-md shadow shadow-slate-900/40"
+            initial="hidden"
+            animate="showing"
+            exit={post.id === postIdRoutingTo ? "showing" : "hidden"}
+            variants={{
+              hidden: {
+                opacity: 0,
+              },
+              showing: {
+                opacity: 1,
+              },
+              whileHover: {
+                // transform: 'translateY(-1px)',
+                top: "-5px",
+                // left: "-5px",
+              },
+            }}
+            whileHover={'whileHover'}
+          >
+            
+            {/** Container around image with gradient/blended background.
+             * 
+            */}
+            <motion.div
+              layoutId={`photo-${post.id}`}
+              className={`relative bg-gradient-to-tr ${post.blend} overflow-hidden rounded-md`}
+              transition={{ ease: "easeOut" }}
+              initial={{ 
+                height: 0,
+              }}
+              animate={{ 
+                height: '150px',
+              }}
+              style={{ originX: 0.5 }}
+            >
+              
+              {/** Image of post.
+               * 
+               */}
+              <motion.img
+                layoutId={`image-${post.id}`}
+                transition={{ ease: "easeInOut" }}
+                src={post.image}
+                alt={post.name}
+                className="absolute w-full object-cover mix-blend-soft-light"
+                initial={{
+                  height: 0,
+                }}
+                animate={{
+                  height: 150,
+                }}
+                style={{
+                  originX: 1,
+                  objectPosition: post.position,
+                }}
+              />
+            </motion.div>
+            
+            {/** Container around Place Name.
+             * 
+            */}
+            <div className="absolute bottom-0 left-0 z-10 pb-4 pl-4">
+              <motion.div
+                layoutId={`title-${post.id}`}
+                transition={{ ease: "easeInOut" }}
+                animate={{ color: "#f8fafc" }}
+              >
+                <h2 className="block text-2xl font-semibold tracking-tighter">
+                  {post.name}
+                </h2>
+              </motion.div>
+            </div>
+          </motion.a>
+        </Link>
       )
     })
-  }
+  )}
+  
   
   return (
     <>
@@ -63,35 +127,35 @@ export default function Page() {
 
 
 
-const testBlogData = [
-  {
-    "id": 1,
-    "title": "Blog Post 1",
-    "slug": "1",
-    "content": "Blog post 1 content.",
-    "author": "Author 1",
-    "date": "2021-01-01",
-    "image" : '/assets/images/places/aspen.jpeg',
-    "blend" : ''
-  },
-  {
-    "id": 2,
-    "title": "Blog Post 2",
-    "slug": "2",
-    "content": "Blog post 2 content.",
-    "author": "Author 2",
-    "date": "2021-01-02",
-    "image" : "/assets/images/places/lake-como.jpeg",
-    "blend" : ''
-  },
-  {
-    "id": 3,
-    "title": "Blog Post 3",
-    "slug": "3",
-    "content": "Blog post 3 content.",
-    "author": "Author 2",
-    "date": "2021-01-02",
-    "image" : "/assets/images/places/nyc.jpeg",
-    "blend" : ''
-  },
-]
+// const testBlogData = [
+//   {
+//     "id": 1,
+//     "title": "Blog Post 1",
+//     "slug": "1",
+//     "content": "Blog post 1 content.",
+//     "author": "Author 1",
+//     "date": "2021-01-01",
+//     "image" : '/assets/images/place/aspen.jpeg',
+//     "blend" : ''
+//   },
+//   {
+//     "id": 2,
+//     "title": "Blog Post 2",
+//     "slug": "2",
+//     "content": "Blog post 2 content.",
+//     "author": "Author 2",
+//     "date": "2021-01-02",
+//     "image" : "/assets/images/place/lake-como.jpeg",
+//     "blend" : ''
+//   },
+//   {
+//     "id": 3,
+//     "title": "Blog Post 3",
+//     "slug": "3",
+//     "content": "Blog post 3 content.",
+//     "author": "Author 2",
+//     "date": "2021-01-02",
+//     "image" : "/assets/images/place/nyc.jpeg",
+//     "blend" : ''
+//   },
+// ]
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link'
 import './globals.css'
+import { AnimatePresence } from 'framer-motion'
 
 export const metadata = {
   title: 'Cross-Route Animation',
@@ -22,35 +23,35 @@ export default function RootLayout({
   // }, []);
 
   return (
-    <html lang="en" className='antialiased h-[100%]'>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        {/* <meta name="color-scheme" content="dark light" /> */}
-        <meta name="description" content={metadata.description} />
-        <meta name="theme-color" content="#000000" />
-        
-        
-        {/* <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/> */}
-        {/* <link rel="manifest" href="/manifest.json" /> */}
+      <html lang="en" className='antialiased h-[100%]'>
+          <meta charSet="utf-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          {/* <meta name="color-scheme" content="dark light" /> */}
+          <meta name="description" content={metadata.description} />
+          <meta name="theme-color" content="#000000" />
+          
+          
+          {/* <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/> */}
+          {/* <link rel="manifest" href="/manifest.json" /> */}
 
-        <title>{metadata.title}</title>
-      <body className={`h-100 min-h-[100%] bg-secondary`}>
-        <header className='bg-secondary p-2'>
-          <Link href="/"><h1>{metadata.title}</h1></Link>
-        </header>
-        
-        {/*//TODO: 20230529 #EP || Add Navigation */}
+          <title>{metadata.title}</title>
+        <body className={`h-100 min-h-[100%] bg-secondary`}>
+          <header className='bg-secondary p-2'>
+            <Link href="/"><h1>{metadata.title}</h1></Link>
+          </header>
+          
+          {/*//TODO: 20230529 #EP || Add Navigation */}
 
-        <main className='h-100vh min-h-[100vh] p-2 py-4'>
-          {children}
-        </main>
-        
-        <footer className='bottom-0 flex text-primary justify-center border-solid border-2 border-b-0 border-l-0 border-r-0 border-blue-700 bg-blue-900 py-2'>
-          Footer Placeholder
-        </footer>
-      </body>
-    </html>
+          <main className='h-100vh min-h-[100vh] p-2 py-4'>
+            {children}
+          </main>
+          
+          <footer className='bottom-0 flex text-primary justify-center border-solid border-2 border-b-0 border-l-0 border-r-0 border-blue-700 bg-blue-900 py-2'>
+            Footer Placeholder
+          </footer>
+        </body>
+      </html>
   )
 }
