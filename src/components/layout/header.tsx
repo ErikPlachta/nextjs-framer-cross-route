1b0f7d31ef6ba8e42c62005984583ec3c3847926
Erik Plachta <eplachta@gmail.com>
2023-06-01 08:00:20 -0400

    Cleanup nav and add hover animations with tw.

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -38,25 +38,21 @@ const navItems: {
 export default function Header({params}:any){
   const pathname  = usePathname()
   return (
-    <header className={`fixed flex justify-center left-0 top-0 py-4 m-auto w-full z-20 backdrop-blur-md bg-white/20 border-solid border-black/10 border-b-2 border-t-0 border-l-0 border-r-0 h-10vh`}>
+    <header className={`fixed flex justify-center left-0 top-0 py-4 m-auto w-full z-20 backdrop-blur-md bg-white/40 border-solid border-black/10 border-b-2 border-t-0 border-l-0 border-r-0`}>
       
       <nav className="flex-auto"> 
         <ul className={`flex flex-row justify-center text-center space-around max-w-[800px] m-auto gap-5 md:gap-10 lg:gap-10 xl:gap-10`}>
           {navItems.map(({ label, page, link, icon }) => (
             page &&
-              <Link className='group flex flex-row w-full gap-2 text-lg tracking-wide' href={page}>
-                <li key={label}
-                    className={`group flex justify-center w-full `
-                          + ` underline-offset-4 hover:underline hover:decoration-blue-600 `
-                  }
-                >
-                  <span className='h-6 w-6 text-blue-400 group-hover:text-blue-500 drop-shadow-sm'>
+              <Link className='group flex flex-row w-full' href={page}>
+                <li key={label} className={`group flex justify-center w-full gap-2`}>
+                  <span className='my-auto h-[20px] w-[20px] text-blue-400 group-hover:text-blue-500 drop-shadow-sm'>
                     {icon}
                   </span>
-                  <span className={
-                      pathname === page
-                        ? ' drop-shadow-sm underline underline-offset-6 hover:decoration-transparent'
-                        : ''
+                  <span className={`text-xl tracking-wide group-hover:underline-offset-4 group-hover:underline `
+                      + `${pathname === page
+                        ? ' drop-shadow-sm underline transition-all decoration-blue-500 underline-offset-8 '
+                        : ' underline-offset-8 decoration-transparent transition-all group-hover:decoration-blue-400'}`
                     }
                   >
                     {label}
